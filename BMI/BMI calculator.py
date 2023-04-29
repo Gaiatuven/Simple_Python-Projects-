7dadcc5e64b968f302f88fcec6e63c0c14ae357a
Gaiatuven <ggwiese@gmail.com>
2023-04-29 16:48:24 +0200

    practice programs

---
+++ BMI/BMI calculator.py
@@ -0,0 +1,22 @@
+print("BMI V2")
+
+height = float(input("Enter your height in m: "))
+weight = int(input("Enter your weight in kg: "))
+
+calculateBMI = weight / (height * height)
+bmi = round(calculateBMI, 2)
+
+if bmi < 18.5:
+    print(f'Your BMI is {bmi}, you are currently underweight')
+
+elif 18.5 <= bmi < 25:
+    print(f'Your BMI is {bmi}, you are at a normal weight')
+
+elif 25 <= bmi < 30:
+    print(f'Your BMI is {bmi}, you are slightly overweight')
+
+elif 30 <= bmi < 35:
+    print(f'Your BMI is {bmi}, you are obese.')
+
+else:
+    print(f'Your BMI is {bmi}, you need help. Clinically obese')
---
+++ leap_year.py/LeapYear.py
@@ -0,0 +1,12 @@
+leap_year = int(input("Which year do you want to check: "))
+
+if leap_year % 4 == 0:
+    if leap_year % 100 == 0:
+        if leap_year % 400 == 0:
+            print("Leap year")
+        else:
+            print("Not a leap year.")
+    else:
+        print("Leap year")
+else:
+    print("Not a leap year.")
